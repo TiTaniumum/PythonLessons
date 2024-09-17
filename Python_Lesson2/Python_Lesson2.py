c58e0d46f942c64f5309923748220ea7eecf2273
Temirlan Yelemanov <36527788+TiTaniumum@users.noreply.github.com>
2024-09-17 19:03:32 +0500

    Lessons 2-4

--- Python_Lesson2/Python_Lesson2.py
+++ Python_Lesson2/Python_Lesson2.py
@@ -1,1 +1,38 @@
+from sys import displayhook
+from tkinter.tix import COLUMN
+import pandas as pd
+import matplotlib.pyplot as plt
+#from IPython.core.display_functions import display
 
+# s1 = pd.Series([1,2,3,4,5,6,7], index=[1,2,3,4,5,6,7])
+
+# print(s1)
+# print(s1.tolist())
+# print(s1.values)
+
+# df = pd.DataFrame([('a','b','c'),('10','20',30)], index=['row1','row2'], columns=['col1','col2','col3'], copy=True)
+# print(df)
+path = r"C:\Users\LEGION\csvfiles" + "\\"
+filename1 = "sample_2_1000.csv"
+df = pd.read_csv(
+    path + filename1, delimiter=";", quotechar='"', encoding="utf-8", header=0
+)
+
+# df['Платеж'] = df['Платеж'].str.replace(',','.').astype(float)
+df['Платеж'] = df['Платеж'].apply(func = lambda x: float(str(x).replace(',','.')))
+# res = df.groupby(['Компания','Область'])['Платеж'].sum()
+# #res = df.groupby(['Компания'])['Платеж'].count()
+# for row, val in res.items():
+#     print(f'{row}: {val}')
+
+результат = df.pivot_table(values=["Платеж"], index="Компания", columns=["Статус"], aggfunc="sum")
+
+результат = df.pivot_table(values=["Платеж"], index="Компания", columns=["Статус"], aggfunc={'Платеж': ['sum','mean']})
+
+print(результат)
+
+# plt.scatter(x=[1,2,3,4,5,6],y=[0.1,0.5,2.0,1.0,-0.6,0.4], s=50, marker='*', c='red')
+# plt.show()
+
+df.groupby(['Компания','Область'])['Платеж'].sum().plot.bar()
+plt.show()
---
+++ Python_Lesson3/main.py
@@ -0,0 +1,30 @@
+name = "Temirlan"
+string = f"My name is {name}"
+# print(string[0:5])
+# print(string[5:])
+# print(string[-5:])
+# print(string[-2])
+# print(string[0:-5])
+# print(string[::-1])
+# a: float
+# b: int = 'qasd'
+# print(type(b))
+# a = 3
+# a = a ** 3
+# a = 3 ^ 3
+# a = 3 | 3
+# a = 3 & 3
+#print(a)
+#print(True)
+
+arr1 = [i for i in range(10)]
+arr2 = [i for i in range(10)]
+#arr1.append('new value')
+#arr1.insert(0,1)
+#print(arr1)
+#arr2 = ['abc', 'def']
+#print(arr1 + arr2)
+#print(arr1 + 'asv')
+#print([1,2,3][0])
+arr3 = (i for i in range(10))
+print()
