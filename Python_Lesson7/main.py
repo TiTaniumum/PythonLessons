638e3d99de9248126ac088f47145299064760a79
Temirlan Yelemanov <36527788+TiTaniumum@users.noreply.github.com>
2024-09-30 19:12:14 +0500

    lessons 7-8

--- Python_Lesson7/main.py
+++ Python_Lesson7/main.py
@@ -40,5 +40,74 @@ def decorator_pretier(func):
 def getTime():
     return time.ctime()
 
-print(getTime())
+#print(getTime())
 
+class Dog:
+    def __init__(self, name):
+        self.name = name
+        print(self)
+    
+    def bark(self):
+        print('bark')
+
+# obj = Dog('karl')
+# obj = object()
+#print(obj)
+
+class Animal: 
+    def __init__(self, name):
+        self.name = name
+
+class Dog(Animal):
+    breed: str
+
+    def __init__(self):
+        super().__init__("name")
+
+    def bark(self):
+        return "Waf Waf!"
+    
+    @staticmethod
+    def static_function():
+        print('static')
+    
+# my_dog = Dog("name")
+# my_dog.bark()
+# Dog.static_function()
+
+class Cat:
+    def meaow(self):
+        self._make_sound()
+    
+    def _make_sound(self): # Protected
+        pass
+
+    def __private_method():
+        pass
+
+class WhiteCat(Cat):
+    def meaow(self):
+        return super().meaow()
+
+class Dog:
+    def __str__(self):
+        return "this is a dog"
+    
+dog = Dog()
+print(dog)
+
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+    
+    def __add__(self, other):
+        return Point(self.x + other.x, self.y + other.y)
+    
+    def __str__(self):
+        return f"x: {self.x}, y: {self.y}"
+    
+point1 = Point(1,2)
+point2 = Point(2,3)
+point3 = point1 + point2
+print(point3)
